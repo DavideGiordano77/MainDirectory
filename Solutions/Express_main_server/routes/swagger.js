d6bc1a6de1ee409bb48ce88ee4ffc58fe720d6df
Davide <davide.giordano77@edu.unito.it>
2025-03-28 10:59:48 +0100

    inizio documentazione swagger (soltanto di prova, da migliorare)

---
+++ Solutions/Express_main_server/routes/swagger.js
@@ -0,0 +1,32 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const path = require('path');
+
+const options = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Movies API',
+            version: '1.0.0',
+            description: 'Documentazione delle API di Express',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000', // URL corretto per il server
+            },
+        ],
+    },
+    apis: [path.join(__dirname, '../routes/*.js')], // Percorso per tutte le rotte
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+const setupSwagger = (app) => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec)); // Documentazione API
+    app.get('/swagger.json', (req, res) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(swaggerSpec);
+    });
+};
+
+module.exports = setupSwagger;
