025f7e8b14a67bdd44aa3db01a4929d2c3f63b6e
Agnese Andriani <agnese.andriani@edu.unito.it>
2025-03-28 12:05:19 +0100

    documentazione swagger

--- Solutions/Express_main_server/routes/chat.js
+++ Solutions/Express_main_server/routes/chat.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+/**
+ * @swagger
+ *  /:
+ *     get:
+ *       summary: Ottiene la pagina principale della chat
+ *       description: Restituisce la pagina della chat con il titolo "My Chat"
+ *       tags:
+ *         - Chat
+ *       responses:
+ *         "200":
+ *           description: Pagina della chat caricata con successo
+ *           content:
+ *             text/html:
+ *               schema:
+ *                 type: string
+ *                 example: "<html>...</html>"
+ *         "500":
+ *           description: Errore nel caricamento della pagina
+ * */
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('pages/chat', { title: 'My Chat' });
--- Solutions/Express_second_server/routes/releases.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const releasesController = require('../controllers/releases');
-
-router.get('/latest-releases', releasesController.getLatestReleases);
-
-module.exports = router;
